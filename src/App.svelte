9dbea312c58d951b4c65eded67f498b752679341
Sam Claus <samclaus@protonmail.com>
2023-03-31 10:23:50 -0400

    Initial app layout (needs work)

--- src/App.svelte
+++ src/App.svelte
@@ -2,7 +2,37 @@
 </script>
 
 <main>
+    <div
+        id="main-toolbar"
+        aria-label="Main toolbar"
+        role="toolbar"
+        aria-orientation="vertical">
+
+    </div>
+    <div id="canvas">
+
+    </div>
 </main>
 
 <style>
+    main {
+        width: 100%;
+        height: 100%;
+
+        display: flex;
+    }
+
+    #main-toolbar {
+        min-width: 80px;
+
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+
+        background-color: white;
+    }
+
+    #canvas {
+        flex: 1 0 0;
+    }
 </style>
